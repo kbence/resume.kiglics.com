9cc00a955403803f144729cc82cee9712d2a4962
Bence Kiglics <bence.kiglics@gmail.com>
2021-03-07 20:15:22 +0100

    Add /usr/bin at the beginning of PATH

--- upload.sh
+++ upload.sh
@@ -7,6 +7,8 @@ set -euo pipefail
 npm run build
 npm run export
 
-env | cut -d = -f 1
+# Ugly hack since something puts /usr/local/bin earlier to PATH
+# than /usr/bin, and there's a buggy version of awscli there.
+export PATH="/usr/bin:$PATH"
 
 aws s3 sync --acl public-read --delete ./out s3://resume.kiglics.com/
