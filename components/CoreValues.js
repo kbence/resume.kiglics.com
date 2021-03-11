0bc695f96e68ed03bed1dcddb7921d177b41fbc7
Bence Kiglics <bence.kiglics@gmail.com>
2021-03-11 10:47:56 +0100

    Reorganized a few things

--- components/CoreValues.js
+++ components/CoreValues.js
@@ -6,19 +6,17 @@ export default class CoreValues extends React.Component {
       <h3>Core values</h3>
       <ul>
         <li>
-          <b>Openness:</b>
-          without new ideas or understanding new perspectives, it is
-          impossible to improve and evolve.
+          <b>Openness:</b> without new ideas or understanding new perspectives,
+          it is impossible to improve and evolve.
         </li>
         <li>
-          <b>Tolerance:</b>
-          I believe that we can only work together on the long run, if
-          we are willing to understand and accept others opinion and identity.
+          <b>Tolerance:</b> I believe that we can only work together on the
+          long run, if we are willing to understand and accept others opinion
+          and identity.
         </li>
         <li>
-          <b>Mutual respect:</b>
-          when we work together, it's important to value each other's
-          work, and their energy and time they put into it.
+          <b>Mutual respect:</b> when we work together, it's important to value
+          each other's work, and their energy and time they put into it.
         </li>
       </ul>
     </div>
--- components/Skills.js
+++ components/Skills.js
@@ -3,17 +3,17 @@ import React from 'react';
 import style from '../styles/Skills.module.css';
 
 const skills = `
-  Linux/Unix, Kubernetes, DevOps,
+  Unix/Linux, Kubernetes, DevOps,
   AWS, Terraform, Ansible, Chef,
   Go, Python, Bash, C/C++, Java
-`.trim().split(',').map(a => a.trim());
+`.trim().split(',').map(skill => skill.trim());
 
 export default class Skills extends React.Component {
   render() {
     return <div>
       <h3>My Skills</h3>
       <div className={style.skills}>
-        {skills.map(a => <span key={a} className={style.skill}>{a}</span>)}
+        {skills.map(skill => <span key={skill} className={style.skill}>{skill}</span>)}
       </div>
     </div>
   }
